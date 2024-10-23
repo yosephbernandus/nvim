999b662a4ddfad5cee9a4148dbd2dc4ab844a56c
yosephbernandus <yosephbernandus@gmail.com>
2024-10-23 20:18:27 +0700

    chore: fixing tab using neo tree buffer

--- init.lua
+++ init.lua
@@ -1265,6 +1265,21 @@ require('lazy').setup({
       -- animation = true,
       -- insert_at_start = true,
       -- …etc.
+      tabpages = true,
+      sidebar_filetypes = {
+        -- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
+        NvimTree = true,
+        -- Or, specify the text used for the offset:
+        undotree = {
+          text = 'undotree',
+          align = 'center', -- *optionally* specify an alignment (either 'left', 'center', or 'right')
+        },
+        -- Or, specify the event which the sidebar executes when leaving:
+        ['neo-tree'] = { event = 'BufWipeout' },
+        -- Or, specify all three
+        Outline = { event = 'BufWinLeave', text = 'symbols-outline', align = 'right' },
+      },
+      separator = { left = '▎', right = '' },
     },
     version = '^1.0.0',
   },
