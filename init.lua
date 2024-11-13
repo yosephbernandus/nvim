45fcc838df5e7ad3ebc19de8e51f89c48c2a1d96
yosephbernandus <yosephbernandus@gmail.com>
2024-11-13 08:03:28 +0700

    chore: image render with kitty

--- init.lua
+++ init.lua
@@ -194,6 +194,20 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Configure window separators
+vim.opt.fillchars:append {
+  horiz = '━',
+  horizup = '┻',
+  horizdown = '┳',
+  vert = '┃',
+  vertleft = '┫',
+  vertright = '┣',
+  verthoriz = '╋',
+}
+
+vim.opt.winblend = 0
+vim.opt.pumblend = 0
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -232,6 +246,44 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+
+  -- kitty image protocol support
+  {
+    '3rd/image.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    opts = {
+      backend = 'kitty', -- Use kitty as the backend
+      max_width = 100, -- Maximum image width
+      max_height = 50, -- Maximum image height
+      max_fps = 30, -- Maximum fps for gifs
+    },
+  },
+
+  -- window picker
+  {
+    's1n7ax/nvim-window-picker',
+    name = 'window-picker',
+    event = 'VeryLazy',
+    version = '2.*',
+    config = function()
+      require('window-picker').setup {
+        autoselect_one = true,
+        include_current = false,
+        filter_rules = {
+          -- filter using buffer options
+          bo = {
+            -- if the file type is one of following, the window will be ignored
+            filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+            -- if the buffer type is one of following, the window will be ignored
+            buftype = { 'terminal', 'quickfix' },
+          },
+        },
+      }
+    end,
+  },
+
   -- Add neo tree nvim
   {
     'nvim-neo-tree/neo-tree.nvim',
@@ -240,12 +292,34 @@ require('lazy').setup({
       'nvim-lua/plenary.nvim',
       'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
       'MunifTanjim/nui.nvim',
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+      '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     opts = {
       filesystem = {
         filtered_items = {
           visible = true,
+        },
+        window = {
+          mappings = {
+            ['<cr>'] = 'open_with_window_picker',
+            ['P'] = { 'toggle_preview', config = { use_float = true } },
+          },
+        },
+        -- Enable preview mode
+        use_libuv_file_watcher = true,
+        preview = {
+          enabled = true,
+          default = 'auto', -- "auto" or "float"
+          show_on_dirs = false,
+          delay = 100,
+          image_enabled = true, -- Enable image preview
+          include_hidden = false,
+          preview_window = {
+            border = 'rounded',
+            row = 1,
+            col = 1,
+            relative = 'cursor',
+          },
         },
       },
     },
@@ -1331,6 +1405,229 @@ require('lazy').setup({
   vim.keymap.set('n', '<A-m>', '<Cmd>BufferScrollRight<CR>', { desc = 'Scroll rigth' }),
 
   -- tab bar
+  -- lualine
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      local lualine = require 'lualine'
+      local colors = {
+        bg = '#202328',
+        fg = '#bbc2cf',
+        yellow = '#ECBE7B',
+        cyan = '#008080',
+        darkblue = '#081633',
+        green = '#98be65',
+        orange = '#FF8800',
+        violet = '#a9a1e1',
+        magenta = '#c678dd',
+        blue = '#51afef',
+        red = '#ec5f67',
+      }
+
+      local conditions = {
+        buffer_not_empty = function()
+          return vim.fn.empty(vim.fn.expand '%:t') ~= 1
+        end,
+        hide_in_width = function()
+          return vim.fn.winwidth(0) > 80
+        end,
+        check_git_workspace = function()
+          local filepath = vim.fn.expand '%:p:h'
+          local gitdir = vim.fn.finddir('.git', filepath .. ';')
+          return gitdir and #gitdir > 0 and #gitdir < #filepath
+        end,
+      }
+
+      -- Config
+      local config = {
+        options = {
+          -- Disable sections and component separators
+          component_separators = '',
+          section_separators = '',
+          theme = {
+            -- We are going to use lualine_c an lualine_x as left and
+            -- right section. Both are highlighted by c theme .  So we
+            -- are just setting default looks o statusline
+            normal = { c = { fg = colors.fg, bg = colors.bg } },
+            inactive = { c = { fg = colors.fg, bg = colors.bg } },
+          },
+        },
+        sections = {
+          -- these are to remove the defaults
+          lualine_a = {},
+          lualine_b = {},
+          lualine_y = {},
+          lualine_z = {},
+          -- These will be filled later
+          lualine_c = {},
+          lualine_x = {},
+        },
+        inactive_sections = {
+          -- these are to remove the defaults
+          lualine_a = {},
+          lualine_b = {},
+          lualine_y = {},
+          lualine_z = {},
+          lualine_c = {},
+          lualine_x = {},
+        },
+      }
+
+      -- Inserts a component in lualine_c at left section
+      local function ins_left(component)
+        table.insert(config.sections.lualine_c, component)
+      end
+
+      -- Inserts a component in lualine_x at right section
+      local function ins_right(component)
+        table.insert(config.sections.lualine_x, component)
+      end
+
+      ins_left {
+        function()
+          return '▊'
+        end,
+        color = { fg = colors.blue }, -- Sets highlighting of component
+        padding = { left = 0, right = 1 }, -- We don't need space before this
+      }
+
+      ins_left {
+        -- mode component
+        function()
+          return ''
+        end,
+        color = function()
+          -- auto change color according to neovims mode
+          local mode_color = {
+            n = colors.red,
+            i = colors.green,
+            v = colors.blue,
+            [''] = colors.blue,
+            V = colors.blue,
+            c = colors.magenta,
+            no = colors.red,
+            s = colors.orange,
+            S = colors.orange,
+            [''] = colors.orange,
+            ic = colors.yellow,
+            R = colors.violet,
+            Rv = colors.violet,
+            cv = colors.red,
+            ce = colors.red,
+            r = colors.cyan,
+            rm = colors.cyan,
+            ['r?'] = colors.cyan,
+            ['!'] = colors.red,
+            t = colors.red,
+          }
+          return { fg = mode_color[vim.fn.mode()] }
+        end,
+        padding = { right = 1 },
+      }
+
+      ins_left {
+        -- filesize component
+        'filesize',
+        cond = conditions.buffer_not_empty,
+      }
+
+      ins_left {
+        'filename',
+        cond = conditions.buffer_not_empty,
+        color = { fg = colors.magenta, gui = 'bold' },
+      }
+
+      ins_left { 'location' }
+
+      ins_left { 'progress', color = { fg = colors.fg, gui = 'bold' } }
+
+      ins_left {
+        'diagnostics',
+        sources = { 'nvim_diagnostic' },
+        symbols = { error = ' ', warn = ' ', info = ' ' },
+        diagnostics_color = {
+          error = { fg = colors.red },
+          warn = { fg = colors.yellow },
+          info = { fg = colors.cyan },
+        },
+      }
+
+      -- Insert mid section. You can make any number of sections in neovim :)
+      -- for lualine it's any number greater then 2
+      ins_left {
+        function()
+          return '%='
+        end,
+      }
+
+      ins_left {
+        -- Lsp server name .
+        function()
+          local msg = 'No Active Lsp'
+          local buf_ft = vim.api.nvim_get_option_value('filetype', { buf = 0 })
+          local clients = vim.lsp.get_clients()
+          if next(clients) == nil then
+            return msg
+          end
+          for _, client in ipairs(clients) do
+            local filetypes = client.config.filetypes
+            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+              return client.name
+            end
+          end
+          return msg
+        end,
+        icon = ' LSP:',
+        color = { fg = '#ffffff', gui = 'bold' },
+      }
+
+      -- Add components to right sections
+      ins_right {
+        'o:encoding', -- option component same as &encoding in viml
+        fmt = string.upper, -- I'm not sure why it's upper case either ;)
+        cond = conditions.hide_in_width,
+        color = { fg = colors.green, gui = 'bold' },
+      }
+
+      ins_right {
+        'fileformat',
+        fmt = string.upper,
+        icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
+        color = { fg = colors.green, gui = 'bold' },
+      }
+
+      ins_right {
+        'branch',
+        icon = '',
+        color = { fg = colors.violet, gui = 'bold' },
+      }
+
+      ins_right {
+        'diff',
+        -- Is it me or the symbol for modified us really weird
+        symbols = { added = ' ', modified = '󰝤 ', removed = ' ' },
+        diff_color = {
+          added = { fg = colors.green },
+          modified = { fg = colors.orange },
+          removed = { fg = colors.red },
+        },
+        cond = conditions.hide_in_width,
+      }
+
+      ins_right {
+        function()
+          return '▊'
+        end,
+        color = { fg = colors.blue },
+        padding = { left = 1 },
+      }
+
+      -- Now don't forget to initialize lualine
+      lualine.setup(config)
+    end,
+  },
+  -- end of lualine
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
@@ -1354,3 +1651,17 @@ require('lazy').setup({
 })
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+-- Configure image.nvim settings (after lazy setup)
+require('image').setup {
+  render = {
+    min_padding = 5,
+    show_label = true,
+    use_dither = true,
+    foreground_color = false,
+    background_color = false,
+  },
+  events = {
+    update_on_nvim_resize = true,
+  },
+}
