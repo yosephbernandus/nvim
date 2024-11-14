0547c0846f269afbfbccc1c7812c861e490d6b80
yosephbernandus <yosephbernandus@gmail.com>
2024-11-14 18:38:49 +0700

    adding hover code action parameter

--- init.lua
+++ init.lua
@@ -727,6 +727,8 @@ require('lazy').setup({
           -- or a suggestion from your LSP for this to activate.
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
 
+          map('<leader>K', vim.lsp.buf.hover, '[K] Hover')
+
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
